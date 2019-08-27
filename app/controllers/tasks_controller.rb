0c39c24a0da4cf8d0a6cb38008f467f497667dd6
I.takumi <mikutamayamai@gmail.com>
2019-08-27 03:40:29 +0000

    current user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,12 @@
 class TasksController < ApplicationController
+    before_action :require_user_logged_in
     before_action :set_task, only: [:show, :edit, :update, :destroy]
+
     
     def index
-        @tasks = Task.all
+        if logged_in?
+         @tasks = current_user.tasks
+        end
     end
     
     def show
@@ -13,21 +17,23 @@ class TasksController < ApplicationController
     end
     
     def create
-        @task = Task.new(task_params)
-        
+        @task = current_user.tasks.build(task_params)
         if @task.save
             flash[:success] = 'タスクが正常に投稿されました'
             redirect_to @task
         else
+            @tasks = current_user.tasks.order(id: :desc).page(params[:page])
             flash.now[:danger] = 'タスクが投稿されませんでした'
             render :new
         end
     end
     
     def edit
+
     end
     
     def update
+
         
         if @task.update(task_params)
             flash[:success] = 'タスクは正常に更新されました'
@@ -40,6 +46,7 @@ class TasksController < ApplicationController
     
     def destroy
         @task.destroy
+ 
         
         flash[:success] = 'タスクは正常に削除されました'
         redirect_to tasks_url

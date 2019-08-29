abfdd9fc869fc2a5e75eeaa5276dc85667ad3c93
I.takumi <mikutamayamai@gmail.com>
2019-08-29 08:45:39 +0000

    correct_user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :correct_user, only: [:show, :update, :edit, :destroy]
 
     
     def index
@@ -22,7 +22,6 @@ class TasksController < ApplicationController
             flash[:success] = 'タスクが正常に投稿されました'
             redirect_to @task
         else
-            @tasks = current_user.tasks.order(id: :desc).page(params[:page])
             flash.now[:danger] = 'タスクが投稿されませんでした'
             render :new
         end
@@ -54,12 +53,15 @@ class TasksController < ApplicationController
     
     private
     
-    def set_task
-        @task = Task.find(params[:id])
-    end
-    
     def task_params
         params.require(:task).permit(:content, :status)
+    end
+    
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+            redirect_to root_url
+        end
     end
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  #before_action :require_user_logged_in, only: [:new]
+  
   def new
     @user = User.new
   end
